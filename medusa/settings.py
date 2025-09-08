061bf3ea9fc4b7c090f3dc5b1cee1e7d800dc28a
pnosvwHes <147055952+pnosvwHes@users.noreply.github.com>
2025-09-08 15:26:18 +0330

    add STATIC_ROOT

--- medusa/settings.py
+++ medusa/settings.py
@@ -116,6 +116,7 @@ USE_I18N = True
 # ===================== STATIC & MEDIA =====================
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
