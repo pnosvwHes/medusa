89bbeb6c6afc2a0280ff0bf667a71911183a7e7f
pnosvwHes <147055952+pnosvwHes@users.noreply.github.com>
2025-09-08 11:10:48 +0330

    log

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -3,26 +3,28 @@ import sys, os, traceback
 log_path = '/home/medusabeautyir/repositories/medusa/logs/passenger_startup.log'
 os.makedirs(os.path.dirname(log_path), exist_ok=True)
 
-# مسیر پروژه
-project_home = '/home/medusabeautyir/repositories/medusa'
-if project_home not in sys.path:
-    sys.path.append(project_home)
-
-# مسیر محیط مجازی
-venv_path = '/home/medusabeautyir/virtualenv/repositories/medusa/3.12'
-activate_this = os.path.join(venv_path, 'bin/activate_this.py')
-with open(activate_this) as file_:
-    exec(file_.read(), dict(__file__=activate_this))
-
-# تنظیمات Django
-os.environ['DJANGO_SETTINGS_MODULE'] = 'medusa.settings'
-
 try:
+    # مسیر پروژه
+    project_home = '/home/medusabeautyir/repositories/medusa'
+    if project_home not in sys.path:
+        sys.path.append(project_home)
+
+    # مسیر محیط مجازی
+    venv_path = '/home/medusabeautyir/virtualenv/repositories/medusa/3.12'
+    activate_this = os.path.join(venv_path, 'bin/activate_this.py')
+    with open(activate_this) as file_:
+        exec(file_.read(), dict(__file__=activate_this))
+
+    # تنظیمات Django
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'medusa.settings'
+
     from django.core.wsgi import get_wsgi_application
     application = get_wsgi_application()
+
 except Exception:
     with open(log_path, 'a') as f:
         f.write('==== Passenger startup error ====\n')
         f.write(traceback.format_exc())
         f.write('\n\n')
+    # دوباره پرتاب کن تا Passenger هم متوجه خطا بشه
     raise
