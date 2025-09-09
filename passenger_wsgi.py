dfd0797a7ee8c68fe327ca27bb0f4dc95c59296f
masoud <masoud.hesaraki1985@gmail.com>
2025-09-09 23:13:01 +0330

    edit passenger

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,13 @@
-import os, sys
+import sys, os
 
-sys.path.insert(0, "/home/medusabeautyir/repositories/medusa")
+# مسیر پروژه
+sys.path.insert(0, "/home/medusabeautyir/medusa_app")
+
+# مسیر virtualenv
+activate_this = '/home/medusabeautyir/virtualenv/repositories/medusa/3.12/bin/activate_this.py'
+with open(activate_this) as f:
+    exec(f.read(), dict(__file__=activate_this))
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "medusa.settings")
 
 from django.core.wsgi import get_wsgi_application
