a1ff0aee0209619bec7aaf33412c2bdaf1aadcbc
masoud <masoud.hesaraki1985@gmail.com>
2025-09-11 09:45:28 +0330

    edit again

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,16 +1,17 @@
 import sys
 import os
 
-# 1. مسیر پروژه (جایی که manage.py هست)
-sys.path.insert(0, '/home/medusabeautyir/medusa_app')
+# مسیر پروژه (جایی که manage.py هست)
+sys.path.insert(0, '/home/medusabeautyir/repositories/medusa')
 
-# 2. فعال کردن virtualenv
+# فعال کردن virtualenv
 activate_this = '/home/medusabeautyir/virtualenv/medusa_app/3.12/bin/activate_this.py'
 with open(activate_this) as file_:
     exec(file_.read(), dict(__file__=activate_this))
 
-# 3. ست کردن تنظیمات Django
-os.environ['DJANGO_SETTINGS_MODULE'] = 'medusa.settings'
+# تنظیمات Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medusa.settings')
 
-# 4. ایمپورت و آماده‌سازی WSGI
-from medusa.wsgi import application
+# ایمپورت و آماده‌سازی WSGI
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
