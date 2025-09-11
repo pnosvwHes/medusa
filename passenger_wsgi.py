5960785bcf6c25e5656936294eaeec0fa02f0c64
masoud <masoud.hesaraki1985@gmail.com>
2025-09-11 11:23:13 +0330

    edit

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,10 +2,10 @@ import sys
 import os
 
 # مسیر پروژه (جایی که manage.py هست)
-sys.path.insert(0, '/home/medusabeautyir/repositories/medusa')
+sys.path.insert(0, '/home/medusabeautyir/medusa')
 
 # فعال کردن virtualenv
-activate_this = '/home/medusabeautyir/virtualenv/medusa_app/3.12/bin/activate_this.py'
+activate_this = '/home/medusabeautyir/virtualenv/medusa/3.12/bin/activate_this.py'
 with open(activate_this) as file_:
     exec(file_.read(), dict(__file__=activate_this))
 
