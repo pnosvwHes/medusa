1abad33bc8f7b2fbc5f342a0208119f28694cd44
masoud <masoud.hesaraki1985@gmail.com>
2025-09-24 14:01:32 +0330

    add PreventDoubleSubmitMiddleware

--- app/middleware.py
+++ app/middleware.py
@@ -1,7 +1,8 @@
 from django.shortcuts import redirect
 from django.conf import settings
 from django.urls import reverse, NoReverseMatch
-# app/middleware.py
+import hashlib
+import time
 
 import logging
 import traceback
@@ -46,5 +47,27 @@ class LoginRequiredMiddleware:
         # اگر کاربر لاگین نکرده و مسیرش آزاد نیست
         if not request.user.is_authenticated and request.path not in allowed_urls:
             return redirect(settings.LOGIN_URL)
+
+        return self.get_response(request)
+
+
+
+class PreventDoubleSubmitMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if request.method == "POST":
+            post_data = str(request.POST) + request.path
+            hash_key = hashlib.md5(post_data.encode()).hexdigest()
+
+            last_key = request.session.get("last_post_key")
+            last_time = request.session.get("last_post_time", 0)
+
+            if last_key == hash_key and time.time() - last_time < 3:
+                return redirect(request.path)
+
+            request.session["last_post_key"] = hash_key
+            request.session["last_post_time"] = time.time()
 
         return self.get_response(request)
--- medusa/settings.py
+++ medusa/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'app.middleware.LoginRequiredMiddleware', 
+    "app.middleware.PreventDoubleSubmitMiddleware",
 ]
 
 ROOT_URLCONF = 'medusa.urls'
